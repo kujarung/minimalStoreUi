63923a8698ed0829a4d616f6c07fc3fc387c44b8
주보화 <rumi@bliex.com>
2020-06-08 12:43:34 +0900

    rules

--- src/views/login/SignUp.vue
+++ src/views/login/SignUp.vue
@@ -1,47 +1,72 @@
 <template>
   <section class="sign-up">
-    <div class="sign-wrap">
-      <div class="sign-title">회원가입</div>
-      <v-text-field solo flat v-model="userObj.USER_NAME" placeholder="이름" class="custom-input"></v-text-field>
-      <v-text-field solo flat v-model="userObj.USER_ID" placeholder="아이디" class="custom-input">
-        <template #append>
-          <v-btn text tile color="#ff0000" @click="select">중복확인</v-btn>
-        </template>
-      </v-text-field>
-      <v-text-field
-        solo
-        flat
-        v-model="userObj.USER_PASSWORD"
-        placeholder="비밀번호"
-        class="custom-input"
-      ></v-text-field>
-      <v-text-field
-        solo
-        flat
-        v-model="userObj.USER_PASSWORD_CHECK"
-        placeholder="비밀번호 확인"
-        class="custom-input"
-      ></v-text-field>
-      <v-text-field
-        solo
-        flat
-        v-model="userObj.USER_PHONE"
-        type="number"
-        placeholder="휴대전화"
-        class="custom-input"
-      ></v-text-field>
-      <v-text-field solo flat v-model="userObj.USER_EMAIL" placeholder="이메일" class="custom-input"></v-text-field>
-      <v-btn
-        block
-        height="48"
-        tile
-        :disabled="!isFormCheck"
-        depressed
-        class="custom-pink-btn mt20"
-        @click="insert"
-      >회원가입</v-btn>
-    </div>
-    <v-btn @click="selectAll">select All</v-btn>
+    <v-form ref="form" v-model="valid">
+      <div class="sign-wrap">
+        <div class="sign-title">회원가입</div>
+        <v-text-field
+          solo
+          flat
+          v-model="userObj.USER_NAME"
+          :rules="nameRules"
+          placeholder="이름"
+          class="custom-input"
+        ></v-text-field>
+        <v-text-field
+          solo
+          flat
+          v-model="userObj.USER_ID"
+          :rules="idRules"
+          placeholder="아이디"
+          class="custom-input"
+        >
+          <template #append>
+            <v-btn text tile color="#ff0000" @click="select">중복확인</v-btn>
+          </template>
+        </v-text-field>
+        <v-text-field
+          solo
+          flat
+          v-model="userObj.USER_PASSWORD"
+          :rules="passwordRules"
+          placeholder="비밀번호"
+          class="custom-input"
+        ></v-text-field>
+        <v-text-field
+          solo
+          flat
+          v-model="userObj.USER_PASSWORD_CHECK"
+          placeholder="비밀번호 확인"
+          :rules="checkPasswordRules"
+          class="custom-input"
+        ></v-text-field>
+        <v-text-field
+          solo
+          flat
+          v-model="userObj.USER_PHONE"
+          type="number"
+          placeholder="휴대전화"
+          class="custom-input"
+        ></v-text-field>
+        <v-text-field
+          solo
+          flat
+          v-model="userObj.USER_EMAIL"
+          :rules="emailRules"
+          placeholder="이메일"
+          class="custom-input"
+        ></v-text-field>
+        <v-btn
+          block
+          height="48"
+          tile
+          :disabled="!valid"
+          depressed
+          class="custom-pink-btn mt20"
+          @click="insert"
+        >회원가입</v-btn>
+      </div>
+      <!-- <v-btn @click="selectAll">select All</v-btn> -->
+    </v-form>
   </section>
 </template>
 
@@ -58,34 +83,60 @@ export default {
         USER_PHONE: "",
         USER_EMAIL: ""
       },
-      isDuplicateOK: false
+      valid: false,
+      isDuplicateOK: false,
+      idRules: [
+        v =>
+          /^[a-z]+[a-z0-9]{5,19}$/.test(v) ||
+          "아이디는 영문자로 시작하는 6~20자 영문자 또는 숫자이어야 합니다."
+      ],
+      nameRules: [
+        v => !!v || "이름을 입력해주세요",
+        v => (v && v.length <= 12) || "이름은 12자 이하로 입력해주세요."
+      ],
+      emailRules: [
+        v => !!v || "이메일을 입력해주세요",
+        v => /.+@.+\..+/.test(v) || "이메일 형식이 옳지 않습니다."
+      ],
+      passwordRules: [
+        v => !!v || "비밀번호를 입력해주세요",
+        v =>
+          /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,16}$/.test(v) ||
+          "대 소문자, 숫자 포함 8자리 이상이어야 합니다."
+      ],
+      checkPasswordRules: [
+        v => !!v || "비밀번호 확인을 입력해주세요",
+        v =>
+          this.password === this.checkPassword ||
+          "비밀번호가 일치하지 않습니다."
+      ]
     };
   },
   mounted() {},
   computed: {
-    isFormCheck() {
-      //폼 내용이 다 입력되었는지 체크
-      if (
-        this.userObj.USER_NAME &&
-        this.userObj.USER_ID &&
-        this.userObj.USER_PASSWORD &&
-        this.userObj.USER_PASSWORD_CHECK &&
-        this.userObj.USER_PHONE &&
-        this.userObj.USER_EMAIL
-      ) {
-        return true;
-      } else return false;
-    },
-    isPasswordCheck() {
-      //패스워드가 서로 같은지 체크
-      if (this.userObj.USER_PASSWORD == this.userObj.USER_PASSWORD_CHECK) {
-        return true;
-      } else return false;
-    }
+    // isFormCheck() {
+    //   //폼 내용이 다 입력되었는지 체크
+    //   if (
+    //     this.userObj.USER_NAME &&
+    //     this.userObj.USER_ID &&
+    //     this.userObj.USER_PASSWORD &&
+    //     this.userObj.USER_PASSWORD_CHECK &&
+    //     this.userObj.USER_PHONE &&
+    //     this.userObj.USER_EMAIL
+    //   ) {
+    //     return true;
+    //   } else return false;
+    // },
+    // isPasswordCheck() {
+    //   //패스워드가 서로 같은지 체크
+    //   if (this.userObj.USER_PASSWORD == this.userObj.USER_PASSWORD_CHECK) {
+    //     return true;
+    //   } else return false;
+    // }
   },
   methods: {
     async insert() {
-      if (this.isDuplicateOK && this.isFormCheck && this.isPasswordCheck) {
+      if (this.valid && this.isDuplicateOK) {
         try {
           const data = await axios({
             method: "get",
@@ -94,15 +145,13 @@ export default {
           }).then(res => {
             console.log("res::", res);
             console.log("insert!");
-            this.$router.push('/');
+            this.$router.push("/");
           });
         } catch (e) {
           console.log(error);
         }
       } else if (!this.isDuplicateOK) {
         alert("아이디 중복확인을 해주세요.");
-      } else if (!this.isPasswordCheck) {
-        alert("패스워드가 같은지 확인해주세요.");
       }
     },
     async select() {
@@ -137,16 +186,7 @@ export default {
         url: "http://localhost:3000/signUp/selectAll"
       });
       console.log(data);
-    }
-
-    //   async select() {
-    //   const data = await axios({
-    //     method: 'get',
-    //     url: 'http://localhost:3000/users/select',
-    //     params : {searchId : 3}
-    //   })
-    //   console.log(data)
-    // },
+    },
   }
 };
 </script>
