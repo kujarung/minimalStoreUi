adc4e6ca13e4f224c2347bed3dc6c9778c6c514c
kujarung <ryan@bliex.com>
2020-06-12 13:30:44 +0900

    3자리 콤마등 추가

--- src/mixin/index.js
+++ src/mixin/index.js
@@ -1,9 +1,16 @@
 const common = {
   data() {
     return {
+      // 파일 패스 잡는 부분
       path : process.env.NODE_ENV === "development" ? "http://localhost:8080" : '/',
     }
-  }
+  },
+  methods: {
+    // 3자리 콤마 (ex. 1000 => 1,000)
+    numberWithCommas(x) {
+      return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+  },
 }
 
 export default common
