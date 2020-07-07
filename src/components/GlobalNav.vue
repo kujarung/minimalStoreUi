86ede2cb90cac95021024466cb3678a04694bcab
주보화 <rumi@bliex.com>
2020-07-07 15:43:02 +0900

    gnb

--- src/components/GlobalNav.vue
+++ src/components/GlobalNav.vue
@@ -1,26 +1,31 @@
 <template>
   <div>
     <v-app-bar color="white" elevation="0" class="px30">
-      <v-toolbar-title class="font-weight-bold font20 mont">
+      <v-toolbar-title class="font-weight-bold font20 mont mr20">
         <router-link to="/">Minimal Store</router-link>
       </v-toolbar-title>
       <router-link to="/product/list">
-        <div class="ml32 mont mt6 hover-underline">Store</div>
+        <div class="menu-list-item">Store</div>
       </router-link>
       <router-link to="/about">
-        <div class="ml32 mont mt6 hover-underline">About</div>
+        <div class="menu-list-item">About</div>
       </router-link>
       <v-spacer></v-spacer>
       <v-sheet max-width="240" min-width="240">
-     <v-text-field class="custom-input"  placeholder="검색어를 입력해 주세요" append-icon="mdi-magnify" hide-details></v-text-field>
-     </v-sheet>
+        <v-text-field
+          class="custom-input"
+          placeholder="검색어를 입력해 주세요"
+          append-icon="mdi-magnify"
+          hide-details
+        ></v-text-field>
+      </v-sheet>
       <div class="d-flex" v-if="userInfo.token.length == 0">
-        <div class="mx20">
-          <router-link to="/login">Login</router-link>
-        </div>
-        <div class="mr20">
-          <router-link to="/user/signUp">Sign Up</router-link>
-        </div>
+        <router-link to="/login">
+          <div class="menu-list-item">Login</div>
+        </router-link>
+        <router-link to="/login">
+          <div class="menu-list-item">Sign Up</div>
+        </router-link>
       </div>
       <template v-else>
         <UserProfile :profileURL="profileURL" />
